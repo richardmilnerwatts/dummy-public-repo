7a6db395cf1b227d0af6754e297089f868d99284
Richard Milner-Watts <milnerwa@amazon.com>
2022-12-13 14:57:50 +0000

    test trigger webhook

---
+++ main.tf
@@ -0,0 +1,18 @@
+# dummy file
+
+provider "aws" {
+   region = "eu-west-1"
+}
+
+# Data Source (https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity)
+data "aws_caller_identity" "current" {}
+
+##########################
+## Variables
+##########################
+
+variable "stack_prefix" {
+    description = "Identifier to prepend to resources"
+    type        = string
+    default     = "reverse-proxy"
+}
